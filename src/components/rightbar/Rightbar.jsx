25fc9614a66ea3a6e56b9d6eeab3e743bc2ffe5b
dnozaki <dnozaki88888888@gmail.com>
2023-07-17 16:57:11 +0900

    implement Rightbar for profile use HTML

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -35,7 +35,44 @@ export default function Rightbar({ profile }) {
   const ProfileRightBar = () => {
     return (
       <>
-        Profile のRightbar です。
+        <h4 className="rightbarTitle">ユーザー情報</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">出身：</span>
+            <span className="rightbarInfoKey">福岡</span>
+          </div>
+          <h4 className="rightbarTitle">あなたの友達</h4>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img src="./assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+              <span className="rightbarFollowingName">Shin Code</span>
+            </div>
+          </div>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img src="./assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+              <span className="rightbarFollowingName">Yamaki</span>
+            </div>
+          </div>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img src="./assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+              <span className="rightbarFollowingName">Koga</span>
+            </div>
+          </div>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img src="./assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+              <span className="rightbarFollowingName">Matsukubo</span>
+            </div>
+          </div>
+          <div className="rightbarFollowings">
+            <div className="rightbarFollowing">
+              <img src="./assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+              <span className="rightbarFollowingName">Ohta</span>
+            </div>
+          </div>
+        </div>
       </>
     )
   }
