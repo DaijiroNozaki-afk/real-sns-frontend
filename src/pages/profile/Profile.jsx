e39f94bf4651c57a5eb6e37f4ec55b1678de2555
dnozaki <dnozaki88888888@gmail.com>
2023-07-17 12:02:32 +0900

    implement profilePage with HTML

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -11,8 +11,23 @@ export default function Profile() {
       <Topbar />
       <div className="profile">
         <Sidebar />
-        <TimeLine />
-        <Rightbar />
+        <div className='profileRight'>
+            <div className="profileRightTop">
+                <div className="profileCover">
+                    <img src="./assets/post/3.jpeg" alt="" className='profileCoverImg' />
+                    <img src="./assets/person/1.jpeg" alt="" className="profileUserImg" />
+                </div>
+                <div className="profileInfo">
+                    <h4 className='profileInfoName'>Shin Code</h4>
+                    <span className="profileInfoDesc">Udemy 講師をしております。</span>
+                </div>
+
+            </div>
+            <div className="profileRightBottom">
+                <TimeLine />
+                <Rightbar />
+            </div>
+        </div>
       </div>
     </>
   )
