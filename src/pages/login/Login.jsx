ede93505af6f75f255a809a584af1b3fd81d3d82
dnozaki <dnozaki88888888@gmail.com>
2023-07-31 16:21:15 +0900

    implement login validation u101

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -11,9 +11,18 @@ export default function Login() {
             </div>
             <div className="loginRight">
                 <div className="loginBox">
-                    <p className="loginMsg">ログインはこちら</p>
-                    <input type="text" className="loginInput" placeholder='Eメール' />
-                    <input type="text" className="loginInput" placeholder='パスワード' />
+                    <p className="loginMsg">ログインはこちらです</p>
+                    <input 
+                        type="email" 
+                        className="loginInput" 
+                        placeholder='Eメール' 
+                        required />
+                    <input 
+                        type="password" 
+                        className="loginInput" 
+                        placeholder='パスワード'
+                        minLength="6" 
+                        required />
                     <button className="loginButton">ログイン</button>
                     <span className="loginForgot">パスワードを忘れた方へ</span>
                     <button className="loginRegisterButton">アカウント作成</button>
