15ae84a24e689c51c0b62bc17da65a894109474c
dnozaki <dnozaki88888888@gmail.com>
2023-08-02 11:19:24 +0900

    ontimeLogin u117

--- src/state/AuthContext.js
+++ src/state/AuthContext.js
@@ -1,27 +1,38 @@
-import { createContext, useReducer } from "react"
-import AuthReducer from "./AuthReducer"
+import { createContext, useReducer } from 'react';
+import AuthReducer from './AuthReducer';
 
 //最初のユーザー状態を定義
 const initialState = {
-    user: null,
-    isFetching: false,
-    error: false,
-}
+  // user: null,
+  user: {
+    _id: '64a4df423074c2dfa5b8dcaa',
+    username: 'shincode',
+    email: 'dnozaki88888888@gmail.com',
+    password: 'Shikurakura8',
+    profilePicture: '',
+    coverPicture: '',
+    followers: [],
+    followings: [],
+    isAdmin: false,
+  },
+  isFetching: false,
+  error: false,
+};
 //状態をグローバルに管理する
-export const AuthContext = createContext(initialState)
+export const AuthContext = createContext(initialState);
 
 export const AuthContextProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(AuthReducer, initialState) 
-    return (
-        <AuthContext.Provider
-            value={{
-                user: state.user,
-                isFetching: state.isFetching,
-                error: state.error,
-                dispatch,
-            }}
-        >
-            {children} //←APP /に相当する
-        </AuthContext.Provider>
-    )
-}
+  const [state, dispatch] = useReducer(AuthReducer, initialState);
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children} //←APP /に相当する
+    </AuthContext.Provider>
+  );
+};
