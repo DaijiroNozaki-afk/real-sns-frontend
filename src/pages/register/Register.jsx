5e9e2563b165754d5458e06d8a95b2c807ec4d9d
dnozaki <dnozaki88888888@gmail.com>
2023-08-02 10:24:48 +0900

    fix registerComponent u112

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./Register.css"
 
 export default function Register() {
+    const username = useRef()
+    const email = useRef()
+    const password = useRef()
+    const passwordConfirmation = useRef()
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
   return (
     <div className='login'>
         <div className="loginWrapper">
@@ -10,15 +18,29 @@ export default function Register() {
                 <span className="loginDesc">本格的なSNS を自分の手で。</span>
             </div>
             <div className="loginRight">
-                <div className="loginBox">
+                <form className="loginBox" onSubmit={(e) => handleSubmit(e)}>
                     <p className="loginMsg">新規登録はこちら</p>
-                    <input type="text" className="loginInput" placeholder='ユーザー名' />
-                    <input type="text" className="loginInput" placeholder='Eメール' />
-                    <input type="text" className="loginInput" placeholder='パスワード' />
-                    <input type="text" className="loginInput" placeholder='確認用パスワード' />
-                    <button className="loginButton">サインアップ</button>
+                    <input type="text" className="loginInput" placeholder='ユーザー名'
+                        required />
+                    <input type="email" 
+                        className="loginInput" 
+                        placeholder='Eメール'
+                        required />
+                    <input 
+                        type="password" 
+                        className="loginInput" 
+                        placeholder='パスワード'
+                        required
+                        minLength="6" />
+                    <input 
+                        type="password" 
+                        className="loginInput" 
+                        placeholder='確認用パスワード'
+                        required
+                        minLength="6" />
+                    <button className="loginButton" type='submit'>サインアップ</button>
                     <button className="loginRegisterButton">ログイン</button>
-                </div>
+                </form>
             </div>
         </div>
     </div>
